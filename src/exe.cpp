f2da8c2e200ed839654bccc8bf47b5f6d6e013c8
Marek Veselý <me@notmarek.com>
2024-08-20 10:35:49 +0200

    Change korepi role to 31 (0b11111)

--- src/exe.cpp
+++ src/exe.cpp
@@ -13,6 +13,10 @@
 #include <cinttypes>
 #include "util.h"
 
+#ifdef CRACKPIPEDEV
+#include "Zydis/Zydis.h"
+#endif
+
 // well-known sigs
 #define KACTR "40 53 55 56 57 41 54 41 56 41 57 48 83 EC ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 44 24 ? 49 8B F1"
 #define SYSERR_SIG "48 89 5C 24 ? 57 48 83 EC ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 44 24 ? 48 8B DA 48 8B F9"
@@ -293,7 +297,7 @@ const std::string verinfo = R"|({
     },
     "sign2": "eMDldspy36kqMIRWWiGE/3J2e6/KAWdC8heSec80zZV8Ck2Z6mesGxGM8hPPkJODChzi8fA6xILl1VdNrZcG7saYa3TL/cyngmiofl0ZO52gepyMqQTY9b91iV1cfPa4SiRaNIag/l/5yAXaCLIyd5SkJ5ie3zu8xw9Pc9UM0CAGcdOO8HlnQNzRyoUtJoGcezdio7rsX/bLPbKC7zx7V1na7y9HULBKjQ1ysDJaRhBFpUthDKD5DZS4zzSWOnST5nc129X/XDDL9H9taRrUwECPnMe1dKjW/dvReKpheimmPLPYr425kTKPUbOh/wVVJuPR0cstrikuDrvxx4JmOw=="
 })|";
-const std::string resp = R"({"msg": "AtZAfcAb+qtSipkXI9CP8u5XUYPGyCbGq5C/VYyt6tcelFYehMuYs0q8m/q+RwGx0/jOB3jDRAqjcqmunJpoKrIFV9W/YC9wzY+GaSU2L8oNQHlpx9KgJ0K50aqwxQD0dKiWmd16b76sLCn8GvpVrSk1k6SoFtUtPe30Cf1BkOsFD2oxSGBioUK22MkPFO2uj5xIXfZ5tC1dB4cS5ttzVlDiLPXY1hlJBqgFpZTj8znRz5qpMhflK5euefmKRPTKzwt+JHFF2YImsmDf49bMCgS6ZIwHL/jbK8dRJwFRjfkZjvpw2XxrL3wKubLqZKjUG3lHP6oKijmcWFTeu68xHphRKmqy43Gg3MZ1wCoYwcQL6tPPoqMy6TJwJdt/mBfhklPRq0XcTAjpnTIJeIo7zH/L1kFaGRAVFtqbwGLIIN08bb+7/tV3MOOc8BEp4RCb721hakBRNFqJAeYrt7yzr/VeK2igLuByrTcBkd0SOIB5LgI5K/qrMf/90bB8sfcicIgJXSVxyuuov45UXM2Rdo4YiL5M8b4LCJwhEkmplS8=", "code": 200})";
+    const std::string resp = R"({"msg": "AtZAfcAb+qtSipkXI9CP8u5XUYPGyCbGq5C/VYyt6tcelFYehMuYs0q8m/q+RwGx0/jOB3jDRAqjcqmunJpoKrIFV9W/YC9wzY+GaSU2L8oNQHlpx9KgJ0K50aqwxQD0dKiWmd16b76sLCn8GvpVrSk1k6SoFtUtPe30Cf1BkOsFD2oxSGBioUK22MkPFO2uj5xIXfZ5tC1dB4cS5ttzVlDiLPXY1hlJBqgFpZTj8znRz5qpMhflK5euefmKRPTKzwt+JHFF2YImsmDf49bMCgS6ZIwHL/jbK8dRJwFRjfkZjvpw2XxrL3wKubLqZKjUG3lHP6oKijmcWFTeu68xHphRKmqy43Gg3MZ1wCoYwcQL6tPPoqMy6TJwJdt/mBfhklPRq0XcTAjpnTIJeIo7zH/L1kFaGRAVFtqbwGLIIN08bb+7/tV3MOOc8BEp4RCb721hakBRNFqJAeYrt7yzr/VeK2igLuByrTcBkd0SOIB5LgI5K/qrMf/90bB8sfcicIgJXSVxyuuov45UXM2Rdo4YiL5M8b4LCJwhEkmplS8=", "code": 200})";
 const std::string oldresp =
         R"({"msg": "vpJSftgQ2noDAZR3Iri/ForvdhDZvxwlJCXowV9TgKSs+BoMyBMOIuxjpDcMTSov1thaXhg/d9aAKcpxOP6glQ3bSd8bHIGMku3Ck/33VdYhtzx4HwC4Lel5mVGZ9+2jffsIgHyIwxMl+8kYwh/QGQRlkC8zFfyNaMszsZiOxIJCy/RMYfI3buvCDPH/4D1/VxysPnaX+QtrVrs7Bt74byqnd38bi0GhpllEWL7CO+7fI+vMe2OSv6s0CUaOqzhDC5N8wIkHsthyVyP+GYoltTov3Bu5iaxmgZc/eYQPTkTWQ759pIVNjKJwnQI3EtOEdrRog6LAkA/CMGwMwBkScvY508Z3KhnNqqIIF9RpYLI6rdST+o2t5gIK4sElQg/2wHZT6wSm23t7YdxnwzEFZysv/H0y63iI4NMUmyZIkRvCyxlWVMpTt/rV9qubdbCjGDxG7A/0LbxCJBfBgEWu4Krpp1S+hk4qgIB+2apCh5sxU76mLzQdFLzNrgmbQADapyDO6rWw777F9FKlo/r9II8kISi/+2FxXp7TZE3ALbcyUo7zKucahsq7u9ucENm64D3PKV4YZCHchQY7xyYI4DaC1PQzleJxGaGbCoBQ0PZK7f33d3N3qB10OaEfe2de4uTcOKbVAjtjSLrlZcMGiZd40Bho76xCtcgAKG2FDxbH/PJo4BoIYwqiDzqpmxXBOsn0JqKLGLaAyU840GAgyLO62lE7/A26w+B9q7hkOIcKlfXZpdwjsll/dADe2U/uF5nrLxEOUGDx9gbUoB95KLD1S3KCCyaLuv8j4imt2E9EgDzk/1XdIwnbPGAECajV5z4yTpMuyD9XBhmJQIFutw==", "code": 200})";
 
@@ -400,8 +404,8 @@ BOOL __stdcall write_process_memory(
                              curl_easy_setopt);
         HOOK_IF_FOUND_MANUAL((uint8_t *) base, expectedRegion, "curl_easy_perform",
                              "40 55 56 48 83 EC 38 48 8B F1 48 85 C9 75 0A 8D", curl_easy_perform);
-        MH_CreateHook((void *) strtoimax, strtoimax_hk, &o_strtoimax_hk);
-        MH_EnableHook((void *) strtoimax);
+//        MH_CreateHook((void *) strtoimax, strtoimax_hk, &o_strtoimax_hk);
+//        MH_EnableHook((void *) strtoimax);
         init = true;
 //        HOOK_IF_FOUND(nullptr, "HWID", "48 89 5C 24 10 48 89 7C 24 18 55 48 8D 6C", krpi_hwid);
 //        if (!o_krpi_hwid) {
@@ -458,7 +462,44 @@ BOOL __stdcall write_process_memory(
 //}
 
 #pragma endregion
+char* base = nullptr;
+void* o_aes_dec;
+void** aes_dec(void** out, void** arg2, void** arg3, void** arg4) {
+    LOG("a2: %s, a3: %s, a4: %s\n", *(char**)arg2, *(char**)arg3, *(char**)arg4);
+    ORIG(aes_dec, out, arg2, arg3, arg4);
+    LOG("out: %s\n", *(char**)out);
 
+    return out;
+}
+
+void* o_parseRes;
+uint64_t parseRes(int64_t* arg1, void** arg2, int64_t arg3, void** arg4, void** key) {
+    LOG("lol: %s %s", *(char**)arg2, *(char**)key);
+//    auto result = ORIG(parseRes, arg1, arg2, arg3, arg4, key);
+//    LOG("REsult: %d %x\n", result, result);
+//    int32_t* role = (int32_t*)(base + 0x56dcbc);
+//    *role = 31;
+    return 0x1;
+}
+#ifdef CRACKPIPEDEV
+void print_zydis(CHAR* cool_shit) {
+    ZyanU64 runtime_address = (ZyanU64)cool_shit;
+    ZyanUSize offset = 0;
+    ZydisDisassembledInstruction instruction;
+    while (ZYAN_SUCCESS(ZydisDisassembleIntel(
+            /* machine_mode:    */ ZYDIS_MACHINE_MODE_LONG_64,
+            /* runtime_address: */ runtime_address,
+            /* buffer:          */ cool_shit + offset,
+            /* length:          */ 32 - offset,
+            /* instruction:     */ &instruction
+    ))) {
+        LOG(" %s\n",  instruction.text);
+
+        offset += instruction.info.length;
+        runtime_address += instruction.info.length;
+    }
+}
+#endif
 void* o_minty_unk;
 void minty_unk(char arg1, void* arg2, int32_t arg3, int64_t arg4, int64_t arg5, int64_t arg6, int64_t arg7, char arg8, char arg9, char arg10, char arg11, char arg12)
 {
@@ -503,7 +544,7 @@ void start() {
         MEMORY_BASIC_INFORMATION mbi;
         bool foundBase = false;
         const auto expectedRegion = 0x3d3000;
-        char *base = nullptr;
+
 
         while (!foundBase) {
             base = nullptr;
@@ -517,17 +558,56 @@ void start() {
             }
         }
         Sleep(500);
-        API_HOOK("kernel32.dll", "WriteProcessMemory", write_process_memory);
+//        API_HOOK("kernel32.dll", "WriteProcessMemory", write_process_memory);
 
         bypass_vmp();
+//        HOOK_IF_FOUND_MANUAL((uint8_t *) base, expectedRegion, "aes", "4C 8B DC 53 56 57 41 54 41 55", aes_dec);
+
+//        BYTE callcode = ((BYTE *)GetProcAddress(ntdll, isWine ? "NtPulseEvent" : "NtQuerySection"))[4]
+        //        89 05 e7 a1 53 00 48 8d 8c 24 40 02 00 00
+        int32_t* role = (int32_t*)(base + 0x56dccc);
+        int32_t* isretard = (int32_t*)(base + 0x56dcc8);
+        *role = 31;
+        *isretard = 0;
+//        void* o_lol;
+//        void* lol = (void*)scan_manual((uint8_t*)base, expectedRegion, "E8 ? ? ? ? 83 3D AF 74 53 00 00");
+//        LOG("lol is at %p\n", lol);
+//        HOOK_IF_FOUND_MANUAL((uint8_t*)base, expectedRegion, "yo", "48 8D 94 24 ? ? ? ? 48 83 BC 24 78 03 00 00 10", lol);
+        HOOK_IF_FOUND_MANUAL((uint8_t *) base, expectedRegion, "parseResponse", "40 53 56 57 41 54 41 55 41 56 41 57 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 84 24 ? ? ? ? 4C 89 8C 24 ? ? ? ?", parseRes);
         HOOK_IF_FOUND_MANUAL((uint8_t *) base, expectedRegion, "curl_easy_setopt",
                              "89 54 24 10 4C 89 44 24 18 4C 89 4C 24 20 48 83 EC 28 48 85 C9",
                              curl_easy_setopt);
+//        BYTE* cool_shit = (BYTE*)scan_manual((uint8_t *) base, expectedRegion,
+//                                             "C7 05 ? ? ? ? ? ? ? ? B9 ? ? ? ?");
+//        LOG("Cool shit @ %p\n", cool_shit);
+//
+//
+//
+//        print_zydis((CHAR*)cool_shit);
+//
+//        BYTE* cool_shit2 = (BYTE*)scan_manual((uint8_t *) base, expectedRegion,
+//                                              "76 04 33 C9");
+//        LOG("Cool shit2 @ %p\n", cool_shit2);
+//        print_zydis((CHAR*)cool_shit2);
+//
+//        BYTE* cool_shit3 = (BYTE*)scan_manual((uint8_t *) base, expectedRegion,
+//                                              "8B 05 ? ? ? ? FF C8");
+//        LOG("Cool shit3 @ %p\n", cool_shit3);
+//        print_zydis((CHAR*)cool_shit3);
+//
+//        BYTE* cool_shit4 = (BYTE*)scan_manual((uint8_t *) base, expectedRegion,
+//                                              "48 83 EC ? 8B 05 ? ? ? ? 85 C0 75 04 33 C9 CD 29 83 3D EF EF 3B 00 01");
+//        LOG("Cool shit4 @ %p\n", cool_shit4);
+//        print_zydis((CHAR*)cool_shit4);
+        // The runtime address (instruction pointer) was chosen arbitrarily here in order to better
+        // visualize relative addressing. In your actual program, set this to e.g. the memory address
+        // that the code being disassembled was read from.
+
 //        API_HOOK("comdlg32.dll", "GetOpenFileNameW", GetOpenFileNameW_hk);
         HOOK_IF_FOUND_MANUAL((uint8_t *) base, expectedRegion, "curl_easy_perform",
                              "40 55 56 48 83 EC 38 48 8B F1 48 85 C9 75 0A 8D", curl_easy_perform);
-        MH_CreateHook((void *) strtoimax, strtoimax_hk, &o_strtoimax_hk);
-        MH_EnableHook((void *) strtoimax);
+//        MH_CreateHook((void *) strtoimax, strtoimax_hk, &o_strtoimax_hk);
+//        MH_EnableHook((void *) strtoimax);
         return;
     };
 
@@ -583,7 +663,6 @@ void start() {
         SUC("Registered VEH for VMP CRC check bypass and/or VEH hooks: %p\n", g_vehUtils.handler);
 
     }
-
 
 //    HOOK_IF_FOUND("MintyZZZ.dll", "system_error", SYSERR_SIG, check_license)
     HOOK_IF_FOUND("MintyZZZ.dll", "Alert", "4C 89 4C 24 ? 53 56 57 48 83 EC ? 41 8B F8", alert);
